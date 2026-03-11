45403a4035b9a82d20ace2ddc6941058e403dc78
Naveed Ur Rahman <rahmannaveed570@gmail.com>
2026-03-11 20:44:28 +0300

    Add model migration and factory of Person

---
+++ app/Models/Person.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Person extends Model
+{
+    /** @use HasFactory<\Database\Factories\PersonFactory> */
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['firstname', 'lastname', 'email', 'phone'];
+}
